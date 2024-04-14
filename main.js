0c886cc64bf68b7470ef6c94dc17e84d26cf133d
aliferoz1234 <m.ali_feroz@hotmail.com>
2024-04-14 12:40:17 +0500

    added a remove option

--- main.js
+++ main.js
@@ -1,21 +1,79 @@
 #! /usr/bin/env node
+// Method 2
 import inquirer from "inquirer";
-let todos = [];
+import chalk from "chalk";
+let todoList = [];
 let condition = true;
-while (condition) {
-    let addTask = await inquirer.prompt([{
-            name: "todo",
-            type: "input",
-            message: "What you want to add in your Todos?"
-        },
-        {
-            name: "addMore",
+while (condition === true) {
+    // --------------------------------------------Options------------------------------------------
+    let option = await inquirer.prompt([{
+            type: "list",
+            name: "userOption",
+            message: "Select an Option",
+            choices: ["Add", "Remove"]
+        }]);
+    // --------------------------------------------Add------------------------------------------
+    if (option.userOption === "Add") {
+        let ans = await inquirer.prompt([{
+                type: "input",
+                name: "userAns",
+                message: "Write something to add in the task list."
+            }]);
+        if (ans.userAns !== "") {
+            todoList.push(ans.userAns);
+            console.log(todoList);
+        }
+        else {
+            console.log(chalk.magenta.bold("Please write something to add in the todo list."));
+        }
+    }
+    // --------------------------------------------Remove------------------------------------------
+    else if (option.userOption === "Remove") {
+        let removeChoice = await inquirer.prompt([{
+                type: "list",
+                name: "removeItem",
+                message: "select item to remove",
+                choices: todoList
+            }]);
+        let indextoRemove = todoList.indexOf(removeChoice.removeItem);
+        if (indextoRemove >= 0) {
+            todoList.splice(indextoRemove, 1);
+            console.log("You removed: ", removeChoice.removeItem);
+            console.log(todoList);
+        }
+    }
+    // --------------------------------------------Confirmation------------------------------------------
+    let userAns = await inquirer.prompt([{
             type: "confirm",
-            message: "Do you want to add more?",
-            default: "false"
-        },
-    ]);
-    todos.push(addTask.todo);
-    condition = addTask.addMore;
-    console.log(todos);
+            name: "selection",
+            message: "Do you want to continue?",
+            default: true
+        }]);
+    if (userAns.selection === false) {
+        condition = false;
+    }
 }
+console.log(chalk.green.bold(`Thank you for using to do list`));
+// Method 1
+// import inquirer from "inquirer"
+// let todos=[];
+// let condition=true;
+// while(condition){
+// let todoQuestions=await inquirer.prompt(
+//     [{
+//         name:"firstQuestion",
+//         type:"input",
+//         message:"What you want to add in your Todos?"
+//     },
+//     {
+//         name:"secondQuestion",
+//         type:"confirm",
+//         message:"Do you want to add more?",
+//         default:"true"
+//     },
+// ]
+// );
+// todos.push(todoQuestions.firstQuestion);
+// condition=todoQuestions.secondQuestion
+// console.log(todos);
+// }
--- main.ts
+++ main.ts
@@ -1,25 +1,89 @@
 #! /usr/bin/env node
-import inquirer from "inquirer"
-let todos=[];
-let condition=true;
+// Method 2
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList:string[]=[];
+let condition:boolean=true;
 
-while(condition){
-let addTask=await inquirer.prompt(
-    [{
-        name:"todo",
-        type:"input",
-        message:"What you want to add in your Todos?"
-    },
-    {
-        name:"addMore",
-        type:"confirm",
-        message:"Do you want to add more?",
-        default:"false"
+while(condition===true){
+    // --------------------------------------------Options------------------------------------------
+    let option=await inquirer.prompt([{
+        type: "list",
+        name:"userOption",
+        message:"Select an Option",
+        choices:["Add","Remove"]
 
-    },
-]
-);
-todos.push(addTask.todo);
-condition=addTask.addMore
-console.log(todos);
+    }])
+    // --------------------------------------------Add------------------------------------------
+    if(option.userOption==="Add"){
+        let ans=await inquirer.prompt([{
+            type:"input",
+            name:"userAns",
+            message:"Write something to add in the task list."
+
+        }])
+        if(ans.userAns!==""){
+           todoList.push(ans.userAns);
+           console.log(todoList);
+        }
+        else{
+            console.log(chalk.magenta.bold("Please write something to add in the todo list."));
+        }
+    }
+// --------------------------------------------Remove------------------------------------------
+        else if(option.userOption==="Remove"){
+            let removeChoice=await inquirer.prompt([{
+                type:"list",
+                name:"removeItem",
+                message:"select item to remove",
+                choices:todoList
+            }]);
+
+            let indextoRemove=todoList.indexOf(removeChoice.removeItem)
+            if(indextoRemove>=0){
+                todoList.splice(indextoRemove,1);
+                console.log("You removed: ",removeChoice.removeItem);
+                console.log(todoList);
+            }
 }
+// --------------------------------------------Confirmation------------------------------------------
+let userAns=await inquirer.prompt([{
+    type:"confirm",
+    name:"selection",
+    message:"Do you want to continue?",
+    default:true
+}])
+if(userAns.selection===false){
+    condition=false;
+}
+}
+console.log(chalk.green.bold(`Thank you for using to do list`));
+
+
+// Method 1
+// import inquirer from "inquirer"
+// let todos=[];
+// let condition=true;
+
+// while(condition){
+// let todoQuestions=await inquirer.prompt(
+//     [{
+//         name:"firstQuestion",
+//         type:"input",
+//         message:"What you want to add in your Todos?"
+//     },
+//     {
+//         name:"secondQuestion",
+//         type:"confirm",
+//         message:"Do you want to add more?",
+//         default:"true"
+
+//     },
+// ]
+// );
+
+// todos.push(todoQuestions.firstQuestion);
+// condition=todoQuestions.secondQuestion
+// console.log(todos);
+// }
+
